98f0bf09d491bbe7f170f728a910b7a1c6843ce8
Aaryan <a.k.65983.586@gmail.com>
2024-02-16 18:47:06 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+print("Welcome to the world of python 😀")
+
+# num1 = int(input())
+# num2 = int(input())
+
+# print(float(num1 + num2))
+
+# string = str(input("Enter your company name -> "))
+# print(string)
+
+# string2 = input("Enter your 2nd company name -> ")
+# print(string2)
